9e2f4f6aee646b9f58416e6dcbfe86226dc18a06
Alen Diaz <noricual@gmail.com>
2021-09-05 11:55:59 -0500

    solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [str, setStr] = useState("");
+  const [result, setResult] = useState("");
+  const [err, setErr] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+
+  const fintNumberOfOcurrences = (mainString) => {
+    if (mainString === "") {
+      setErr(true);
+      setErrMsg(<p className="err"> string cannot be empty. </p>);
+      return;
+    }
+    if (mainString.length < 5) {
+      setErr(true);
+
+      setErrMsg(<p className="err"> string must be at least 5 char long. </p>);
+      return;
+    }
+
+    let occurrences = {};
+    let moreThanOneOccurrence = {};
+
+    for (let i = 0; i < mainString.length; i++) {
+      if (i <= mainString.length - 4) {
+        let newSubstr = mainString.slice(i, i + 4);
+
+        if ([newSubstr] in occurrences) {
+          moreThanOneOccurrence[newSubstr] = occurrences[newSubstr] + 1;
+          occurrences[newSubstr] = occurrences[newSubstr] + 1;
+        } else {
+          occurrences[newSubstr] = 1;
+        }
+      }
+    }
+
+    if (Object.keys(moreThanOneOccurrence).length < 1) {
+      setErr(true);
+      setErrMsg(<p className="err"> No substring apperas more than once. </p>);
+    }
+    setResult(moreThanOneOccurrence);
+
+
+    return moreThanOneOccurrence;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__holder">
+        <h1 className="app__header">Occurrences in a string</h1>
+        <div className="input__container">
+          <label className="label__str" htmlFor="str">
+            Please enter a string:
+          </label>
+          <input
+            autoComplete="off"
+            onChange={(ev) => {
+              setErr(false);
+              setResult("");
+              setStr(ev.target.value);
+            }}
+            className="str__input"
+            name="str"
+          ></input>
+        </div>
+        {err && <div className="result__container">{errMsg}</div>}
+        {result !== "" && (
+          <div className="result__container">
+            <p className="result__object">{JSON.stringify(result)}</p>
+          </div>
+        )}
+      </div>
+      <button onClick={() => fintNumberOfOcurrences(str)} className="btn">
+        Analize
+      </button>
     </div>
   );
-}
+};
 
 export default App;
